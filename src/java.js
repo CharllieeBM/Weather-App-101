b555a258c68d12da934206608a008e1560f9de85
CharllieeBM <cburylb2@gmail.com>
2024-08-07 14:04:33 +0200

    Added time and date element

--- src/java.js
+++ src/java.js
@@ -5,14 +5,39 @@ function updateTempData(response) {
   let descriptionElement = document.querySelector("#condition");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#current-time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
+  timeElement.innerHTML = changedDate(date);
   temperatureElement.innerHTML = Math.round(temperature);
+}
 
-  console.log(response.data.wind.speed);
+function changedDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  return `${day}, ${hours}:${minutes},`;
 }
 
 function searchCity(city) {
@@ -35,35 +60,3 @@ let searchFormElement = document.querySelector("#search-city");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Toronto");
-
-/* function changedDay (date){
-    let hours = date.getHours();  
-    if (hours < 10) {
-      hours = `0${hours}`;
-    } 
-    let minutes = date.getMinutes(); 
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    } 
-    let day = date.getDay();
-    
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
-    ];
-  }
-  
-  let changedDay = days[day];
-  return `${changedDay} ${hours}:${minutes}`;
-}
-
-let currentDate = new Date();
-currentDateELement.innerHTML = changedDay(currentDate);
-
-let searchForm = document.querySelector(".searchCity");
-searchForm.addEventListener("submit", search); */
