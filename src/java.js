01970e1fe5edd0b97bc4776d1247c1587aaf4ba4
CharllieeBM <cburylb2@gmail.com>
2024-08-07 14:37:23 +0200

    added icon

--- src/java.js
+++ src/java.js
@@ -7,7 +7,9 @@ function updateTempData(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#current-time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon-image");
 
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="icon" />`;
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
