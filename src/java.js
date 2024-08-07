91339420dc3fc301d54c3b6773b90f1b6742358c
CharllieeBM <cburylb2@gmail.com>
2024-08-07 11:17:44 +0200

    typed javascript based on knowledge to test it out but it's not working :(

---
+++ src/java.js
@@ -0,0 +1,38 @@
+function search(event) {
+    event.preventDefault();
+    let searchInputElement = document.querySelector("#search-city");
+    let city= document.querySelector("#main-city");
+    city.innerHTML = searchInputElement.value;
+  }
+
+  function changedDay (date){
+    let hours = date.getHours();  
+    if (hours < 10) {
+      hours = `0${hours}`;
+    } 
+    let minutes = date.getMinutes(); 
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    } 
+    let day = date.getDay();
+    
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+  }
+  
+  let changedDay = days[day];
+  return `${changedDay} ${hours}:${minutes}`;
+}
+
+let currentDate = new Date();
+currentDateELement.innerHTML = changedDay(currentDate);
+
+let searchForm = document.querySelector(".searchCity");
+searchForm.addEventListener("submit", search);
